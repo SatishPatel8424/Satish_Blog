8067116cdbe436daf236f094d793689282ed20cc
SatishPatel8424 <satishkotadiya2013@gmail.com>
2020-07-17 14:32:48 +0530

    creating model file and admin side register

---
+++ Blog/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+
+from datetime import date
+from django.urls import reverse
+from django.contrib.auth.models import User #Blog author and commenter
+
+# Blog Author model is here..
+class BlogAuthor(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+    bio = models.TextField(max_length=500, help_text="Please Enter the bio details here.")
+
+    class Meta:
+        ordering = ["user", "bio"]
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular blog-author instance.
+        """
+        return reverse('blogs-by-author', args=[str(self.id)])
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.user.username
+
+
+# Blog model is here..
+class Blog(models.Model):
+
+    name = models.CharField(max_length=500)
+    author = models.ForeignKey(BlogAuthor, on_delete=models.SET_NULL, null=True)
+    description = models.TextField(max_length=5000, help_text="Please Enter the blog description here.")
+    post_date = models.DateField(default=date.today)
+
+    class Meta:
+        ordering = ["-post_date"]
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular blog instance.
+        """
+        return reverse('blog-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.name
+
+# Blog comment model is here..
+class BlogComment(models.Model):
+
+    description = models.TextField(max_length=2000, help_text="Please Enter the comment about blog here.")
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    post_date = models.DateTimeField(auto_now_add=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["post_date"]
+
+    def __str__(self):
+
+        len_title = 75
+        if len(self.description) > len_title:
+            titlestring = self.description[:len_title] + '...'
+        else:
+            titlestring = self.description
+        return titlestring
+
--- Satish_Blog/settings.py
+++ Satish_Blog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Add config for Blog application
+    'Blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
