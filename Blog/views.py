d38af1ec4c99350a6f8e70e00ff8e55476a2a5a0
SatishPatel8424 <satishkotadiya2013@gmail.com>
2020-07-18 10:28:56 +0530

    All Blogs and /blog/blogger/<author-id> to display..

--- Blog/views.py
+++ Blog/views.py
@@ -16,6 +16,48 @@ def index(request):
         request,
         'index.html',
     )
+class BlogListView(generic.ListView):
+    """
+    Generic class-based view for a list of all blogs.
+    """
+    model = Blog
+    paginate_by = 5
+
+class BlogDetailView(generic.DetailView):
+    """
+    Generic class-based detail view for a blog.
+    """
+    model = Blog
+
+
+from django.shortcuts import get_object_or_404
+
+
+class BlogListbyAuthorView(generic.ListView):
+    """
+    Generic class-based view for a list of blogs posted by a particular BlogAuthor.
+    """
+    model = Blog
+    paginate_by = 5
+    template_name = 'blog/blog_list_by_author.html'
+
+    def get_queryset(self):
+        """
+        Return list of Blog objects created by BlogAuthor (author id specified in URL)
+        """
+        id = self.kwargs['pk']
+        target_author = get_object_or_404(BlogAuthor, pk=id)
+        return Blog.objects.filter(author=target_author)
+
+    def get_context_data(self, **kwargs):
+        """
+        Add BlogAuthor to context so they can be displayed in the template
+        """
+        # Call the base implementation first to get a context
+        context = super(BlogListbyAuthorView, self).get_context_data(**kwargs)
+        # Get the blogger object from the "pk" URL parameter and add it to the context
+        context['blogger'] = get_object_or_404(BlogAuthor, pk=self.kwargs['pk'])
+        return context
 
 
 
